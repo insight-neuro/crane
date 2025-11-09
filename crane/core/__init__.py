fc7115c4dd2cb7eb893a4d638408e0e699f127ec
alexander-brady <bradya@student.ethz.ch>
2025-11-09 22:18:30 +0100

    feat: hf-style base classes

---
+++ crane/core/__init__.py
@@ -0,0 +1,10 @@
+from .config import BrainConfig
+from .model import BrainModel, BrainOutput
+from .processing import BrainFeatureExtractor
+
+__all__ = [
+    "BrainConfig",
+    "BrainModel",
+    "BrainOutput",
+    "BrainFeatureExtractor",
+]
