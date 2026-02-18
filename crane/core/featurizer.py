fc60498ff0da6f0cc328fdb307c7ca5ea30eac10
alexander-brady <bradya@student.ethz.ch>
2026-02-18 17:02:20 +0100

    feat: Brain batch objects + featurizer in datasets

--- crane/core/featurizer.py
+++ crane/core/featurizer.py
@@ -1,7 +1,27 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
-from transformers import SequenceFeatureExtractor
+from temporaldata import Data
+from transformers import BatchFeature, SequenceFeatureExtractor
+from transformers.utils.generic import TensorType
+
+
+class BrainFeature(BatchFeature):
+    """
+    BatchFeature subclass for brain data. Can be used to store features extracted from neural recordings.
+
+    This class is derived from a python dictionary and can be used as a dictionary.
+    """
+
+    def __init__(
+        self,
+        data: dict[str, Any],
+        *,
+        tensor_type: None | str | TensorType = None,
+        **kwargs,
+    ):
+        data.update(kwargs)
+        super().__init__(data=data, tensor_type=tensor_type)
 
 
 class BrainFeatureExtractor(SequenceFeatureExtractor, ABC):
@@ -13,10 +33,10 @@ class BrainFeatureExtractor(SequenceFeatureExtractor, ABC):
     Override the `forward` method to implement custom feature extraction logic.
     """
 
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        return self.forward(*args, **kwds)
+    def __call__(self, data: Data, **kwargs) -> BrainFeature:
+        return self.forward(data, **kwargs)
 
     @abstractmethod
-    def forward(self, *args: Any, **kwds: Any) -> Any:
+    def forward(self, data: Data, **kwargs) -> BrainFeature:
         """Forward method to be implemented by subclasses."""
         ...
--- crane/data/data.py
+++ crane/data/data.py
@@ -1,23 +1,26 @@
 from collections.abc import Callable, Iterable
 from pathlib import Path
+from typing import override
 
 from temporaldata import Data
-from torch_brain.dataset import Dataset
+from torch_brain.dataset import Dataset, DatasetIndex
 
+from crane.core.featurizer import BrainFeature, BrainFeatureExtractor
 from crane.data.selectors import Selector, Subjects, SubjectSessions
 
 
 class CraneDataset(Dataset):
-    """Dataset for loading and transforming data from CRANE recordings.
+    """Dataset for loading and transforming Crane-formatted neural recordings.
 
     Args:
-        dataset_dir: Path to the directory containing the CRANE recordings.
+        dataset_dir: Path to the directory containing the neural recordings.
         select: Optional selection criteria for filtering recordings. Either:
             - Selector instance (e.g., Subjects, SubjectSessions, or combinations thereof)
             - List of subject IDs (e.g., [1, 2, 3])
             - List of (subject ID, session ID) pairs (e.g., [(1, 1), (2, 1), (3, 2)])
             - None (default) to include all recordings.
         transform: Optional function to apply to each loaded Data object.
+        featurizer: Optional BrainFeatureExtractor to apply to each loaded Data object.
         keep_files_open: Whether to keep HDF5 files open for faster access (default: True).
     """
 
@@ -26,10 +29,12 @@ class CraneDataset(Dataset):
         dataset_dir: str | Path,
         select: Selector | Iterable[int | tuple[int, int]] | None = None,
         transform: Callable[[Data], Data] | None = None,
+        featurizer: BrainFeatureExtractor | None = None,
         keep_files_open: bool = True,
     ):
         dataset_dir = Path(dataset_dir)
         subject_sessions = sorted(x.stem for x in dataset_dir.glob("sub-*_ses-*.h5"))
+        self.featurizer = featurizer
 
         #  Apply selection criteria
         if select is not None:
@@ -56,3 +61,10 @@ class CraneDataset(Dataset):
             keep_files_open=keep_files_open,
             namespace_attributes=["session.id", "subject.id", "channels.id"],
         )
+
+    @override
+    def __getitem__(self, index: DatasetIndex) -> BrainFeature:  # type: ignore[override]
+        data = super().__getitem__(index)
+        if self.featurizer is not None:
+            data = self.featurizer(data)
+        return BrainFeature(data.data.data)  # type: ignore[attr-defined]
