48f0277c2811ddffa3ac73605c8b5c9871b78225
alexander-brady <bradya@student.ethz.ch>
2026-03-25 16:59:16 +0100

    feat: electrode_subset now allows passing in a specific subset

--- crane/preprocess/electrode_subset.py
+++ crane/preprocess/electrode_subset.py
@@ -1,6 +1,6 @@
 import numpy as np
 from torch import Tensor
-
+from typing import Sequence
 from ..data.structures import ChannelDict
 
 
@@ -8,7 +8,9 @@ def subset_electrodes[T: (np.ndarray, ChannelDict)](
     data: Tensor,
     channels: T,
     max_n_electrodes: int,
+    *,
     batch_first: bool = True,
+    subset: Sequence[int | str] | None = None,
 ) -> tuple[Tensor, T]:
     """
     Subset the electrodes to a random selection of electrodes. Consistent across a batch.
@@ -19,17 +21,31 @@ def subset_electrodes[T: (np.ndarray, ChannelDict)](
         channel_ids (np.ndarray | ChannelDict): Array of channel IDs or ChannelDict
         max_n_electrodes (int): Maximum number of electrodes to keep.
         batch_first (bool): Whether the batch dimension is the first dimension in the data tensor.
+        subset (Sequence[int | str] | None): Optional list of electrode indices or IDs to subset to. If None, a random subset is chosen.
 
     Returns:
         tuple[torch.Tensor, np.ndarray | ChannelDict]: Subsetted data tensor and corresponding channel IDs.
     """
+    # Determine indices to subset
+    if not subset:
+        if len(channels) <= max_n_electrodes:
+            return data, channels
+        indices = np.random.choice(len(channels), max_n_electrodes, replace=False)
 
-    if len(data) > max_n_electrodes:  # Else no-op
-        selected_indices = np.random.choice(len(channels), max_n_electrodes, replace=False)
-        if batch_first:
-            data = data[:, selected_indices, ...]
-        else:
-            data = data[selected_indices, ...]
-        channels = channels[selected_indices]
+    elif all(isinstance(e, str) for e in subset):
+        if not isinstance(channels, ChannelDict):
+            raise ValueError("channels must be a ChannelDict if subset contains channel IDs")
+
+        id_set = set(subset)
+        indices = [i for i, cid in enumerate(channels.id) if cid in id_set]
+    else:
+        indices = np.array(set(subset))
+
+    # Subset data and channels
+    if batch_first:
+        data = data[:, indices, ...]
+    else:
+        data = data[indices, ...]
+    channels = channels[indices]
 
     return data, channels
